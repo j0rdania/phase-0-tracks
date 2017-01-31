a7dbe87ce3e7b6ae9a1280c91c1cfe31feb5088e
Jordan Levine <jordan@strofina.com>
2017-01-30 20:49:58 -0800

    Add comments in data collection area and add structure to ask user for updates

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,20 +1,35 @@
 
 # Designs R Us Client Information Gathering System
 
-# collect each field from screen
+############   collect each field from screen   #####################
 
+# display welcome message
 puts "This utility will collect data for a new client."
+# get first name
 puts "Please enter client's first name: "
 fname=gets.chomp
+# get last name
 puts "Please enter client's last name: "
 lname=gets.chomp
+# get email address
 puts "Please enter client's email address: "
 email=gets.chomp
+# get phone number
 puts "Please enter client's phone number: "
 phone=gets.chomp
+# get number of houses owned; if user hits enter key, set number of houses to nil; otherwise, convert it to integer
 puts "Please enter number of houses client owns: (if unknown, press enter)"
-num_houses=gets.chomp.to_i
-
+num_houses=gets.chomp
+if num_houses.nil? || num_houses == ''
+  num_houses=nil
+else
+  num_houses.to_i
+end
+# find out if client is a minimalist; make sure user enters one of the following acceptable values and set minimalist_pref field accordingly:
+# y or Y: true
+# n or N: true
+# u or U: nil
+# user presses enter key: nil
 puts "Is client a minimalist? (answer Y, N, or U for unknown)"
 answer=gets.chomp.upcase
 valid_answer=false
@@ -36,7 +51,7 @@ until valid_answer
     answer=gets.chomp.upcase
   end
 end
-
+# get number of children; if user hits enter key, set number of children to nil; otherwise, convert it to integer
 puts "How many children does this client have? (if unknown, press enter)"
 num_kids=gets.chomp
 if num_kids.nil? || num_kids==''
@@ -44,7 +59,7 @@ if num_kids.nil? || num_kids==''
 else
   num_kids=num_kids.to_i
 end
-
+# get credit rating; if user hits enter key, set credit rating to nil; otherwise, convert it to integer
 puts "Please enter client's credit rating (if unknown, press enter):"
 cr_rating=gets.chomp
 if cr_rating.nil? || cr_rating==''
@@ -64,3 +79,16 @@ client = {
   number_children: num_kids,
   credit_rating: cr_rating
 }
+puts "Please confirm your entries:"
+puts client
+puts "If you would like to update a field, please enter the field name (enter 'none' if everything looks right)"
+field_to_update=gets.chomp
+if filed_to_update == 'none'
+  # no need to update field
+else
+puts 'add update field code here'
+
+end
+
+puts "Thank you. The following information has been entered:"
+puts client
