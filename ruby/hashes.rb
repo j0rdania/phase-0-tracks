318454f1f212e88aba4a8adef89e838ae10226ce
Jordan Levine <jordan@strofina.com>
2017-01-30 23:17:03 -0800

    Add meat to ask user for modification and add array of house styles

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -23,7 +23,22 @@ num_houses=gets.chomp
 if num_houses.nil? || num_houses == ''
   num_houses=nil
 else
-  num_houses.to_i
+  num_houses = num_houses.to_i
+end
+# fill array with house styles for each house owned by client
+# declare empty array of house styles
+house_type_list=[]
+if !(num_houses.nil?) && (num_houses >= 1)
+  # get style of each house
+  for i in 1..num_houses
+    # get style of house i
+    puts "Please enter style of house " + i.to_s
+    this_style=gets.chomp
+    # populate array with the style of house i
+    house_type_list.push(this_style)
+  end
+else
+  house_type_list=nil
 end
 # find out if client is a minimalist; make sure user enters one of the following acceptable values and set minimalist_pref field accordingly:
 # y or Y: true
@@ -59,6 +74,7 @@ if num_kids.nil? || num_kids==''
 else
   num_kids=num_kids.to_i
 end
+
 # get credit rating; if user hits enter key, set credit rating to nil; otherwise, convert it to integer
 puts "Please enter client's credit rating (if unknown, press enter):"
 cr_rating=gets.chomp
@@ -75,20 +91,56 @@ client = {
   email_address: email,
   phone_number: phone,
   number_of_houses: num_houses,
+  house_styles: house_type_list,
   minimalist: minimalist_pref,
   number_children: num_kids,
   credit_rating: cr_rating
 }
+# display message for user to view his or her entries
 puts "Please confirm your entries:"
 puts client
+# ask user if he or she would like to update a field value
 puts "If you would like to update a field, please enter the field name (enter 'none' if everything looks right)"
 field_to_update=gets.chomp
-if filed_to_update == 'none'
+if field_to_update == 'none'
   # no need to update field
 else
-puts 'add update field code here'
-
+  # ask user for new value for specified field
+  puts "Please enter new value for the " + field_to_update + ' field:'
+  new_value=gets.chomp
+  # convert new_value to proper type, if necessary
+  # note: we assume that user entered a value of the correct type. 
+  case field_to_update
+    when "number_of_houses","number_children","credit_rating"
+      new_value = new_value.to_i
+    when "minimalist"
+      if new_value.nil? || new_value == ''
+        new_value=nil
+      elsif new_value.upcase == 'Y' 
+        new_value=true
+        elseif new_value.upcase == 'N'
+        new_falue=false
+        elseif new_value == 'U'
+        new_value=nil
+      else
+        new_value=nil
+      end
+    when "house_styles"
+      # ask which house entry needs to be changed
+      puts "Please enter the house number that correspons to the new style entered (1 for first house listed, 2 for second house listed, etc.)"
+      house_num=gets.chomp.to_i
+  end
 end
+# set value in hash for specified key
+if field_to_update == 'house_styles'
+  # set appropriate house_styles array entry to new house style
+  house_type_list[house_num]=new_value
+  # load array into new value in preparation for placing in hash
+  new_value=house_type_list
+end
+# set client hash key to new value 
+client[field_to_update.to_sym]=new_value
 
+# thank user and display entered information (with updated field, if applicable)
 puts "Thank you. The following information has been entered:"
 puts client
