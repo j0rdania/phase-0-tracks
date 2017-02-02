030bd4a4816141923ae604834b1dd645bec8e6e6
Jordan Levine <jordan@strofina.com>
2017-02-01 17:20:07 -0800

    Add map features

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,5 +1,6 @@
 
 #release 0
+puts "RELEASE 0"
 #define method that will take block code as argument
 def display_favorite_things
   puts "This method is gearing up to start!"
@@ -9,11 +10,13 @@ def display_favorite_things
   yield("soccer","football","jazzercise")
   puts "This method is all done with work and is winding down."
 end
-
 #call method
 display_favorite_things{|thing1,thing2, thing3| puts "My favorite things are: #{thing1}, #{thing2}, and #{thing3}!"}
+#print blank line
+puts
 
 #release 1 
+puts "RELEASE 1"
 # declare junk food Array
 junk_food = [
   "pizza",
@@ -37,15 +40,21 @@ favorite_foods = {
     trixie: "kibble"
   }
 #iterate through fruits array and print each value 
-puts "Sample fruits:"
+puts "Fruits:"
 fruits.each do |fruit|
   puts "#{fruit}"
 end
+#print hash before.each method call
+puts "Favorite Foods after .each method call:"
+puts favorite_foods
 #iterate through favorite foods hash and print each key/value pair 
 puts "Favorite foods:"
 favorite_foods.each do |person,fave_food|
   puts "#{person} likes #{fave_food}"
 end
+# print array to show fruits current contents
+puts "fruits before .map method call:"
+puts fruits
 #iterate through fruits array and change values in place to junk food
 junk_food_index = -1
 fruits.map! do |fruit|
@@ -54,4 +63,7 @@ fruits.map! do |fruit|
   puts "changing fruit #{fruit} to junk food #{junk_food[junk_food_index]}"
   # set last value to junk food so that fruits are replaced with junk foods
   junk_food[junk_food_index]
-end   
+end    
+# print array to show it has been modified (fruits to junk foods) by .map! method call
+puts "fruits after .map method call:"
+puts fruits
