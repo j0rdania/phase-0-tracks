4816414ea31b3a7955c157913309cd6803615b8a
Jordan Levine <jordan@strofina.com>
2017-02-03 17:23:42 -0800

    Add get next vowel method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -28,6 +28,31 @@ def is_upper? (letter)
 end
 
 def get_next_vowel (vowel_to_change)
+  # record upper case status
+  upper_case_status=is_upper?(vowel_to_change)
+  # convert to lower case for determining next vowel
+  new_letter=''
+  case vowel_to_change
+    when 'a'
+      new_letter='e'
+    when 'e'
+      new_letter='i'
+    when 'i'
+      new_letter='o'
+    when 'o'
+      new_letter='u'
+    when 'u'
+      new_letter='a'
+  end
+  # convert letter back to upcase if it was upper case initially
+  if upper_case_status then
+    new_letter.upcase 
+  else
+    new_letter
+  end 
+end 
+
   # is this letter upper case?
+  # if so, then return upper case version of
 
 end
