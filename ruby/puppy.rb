efe92eef786442e9269f321ceca4782a80b798b4
Jordan Levine <jordan@strofina.com>
2017-02-07 17:59:42 -0800

    simplify ferry rider class

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -78,36 +78,39 @@ trix.eat('kibble')
 
 # ferry rider class
 class Ferry_rider
-  def initialize
+  def initialize (rider_name,introvert_level)
     # this initialize method will print a statement to indicate this passenger is boarding the ferry
-    puts "I am boarding the ferry!"
+    puts "#{rider_name} is boarding the ferry!"
+
   end
 
-  def find_seat(introvert_level,commuter_boat_flag)
-    # this method will inspect the introvert level of this passenger (10 is very introverted, 1 is very extroverted)
-    # and the commuter boat flag (set to true if this is a commuter boat which is normally crowded), and it will 
+  def find_seat
+    # this method will inspect the introvert level of this passenger (10 is very introverted, 1 is very extroverted), and it will 
     # choose the seat that this passenger should look for
-    if commuter_boat_flag
-      return 'take first available seat'
-    else
-      case introvert_level
-        when 1..4
-          return 'sit in galley with talkers'
-        when 5..8
-          return 'sit in back of boat'
-        else
-          return 'go upstairs to quiet room'
-      end
+    case introvert_level
+      when 1..4
+        return 'sit in galley with talkers'
+      when 5..8
+        return 'sit in back of boat'
+      else
+        return 'go upstairs to quiet room'
     end
   end
 
 end
 
-jordan = Ferry_rider.new
-puts jordan.find_seat(5,false)
-mary = Ferry_rider.new
-puts mary.find_seat(8,true)
-tom = Ferry_rider.new
-puts tom.find_seat(9,false)
-simon = Ferry_rider.new
-puts simon.find_seat(1,false)
+jordan = Ferry_rider.new('Jordan',5)
+puts "Seat recommendation for this passenger: " + jordan.find_seat
+mary = Ferry_rider.new('Mary',1)
+puts "Seat recommendation for this passenger: " + mary.find_seat
+tom = Ferry_rider.new('Tom',9)
+puts "Seat recommendation for this passenger: "  + tom.find_seat
+simon = Ferry_rider.new('Simon',2)
+puts "Seat recommendation for this passenger: " + simon.find_seat
+
+
+# this_rider = 0
+# while this_rider < 50
+#   ('rider_' + this_rider.to_s).new
+#   this_rider += 1
+# end
