d78e4cb2228f948d5af0ebaa41d54c5accaffbaa
Jordan Levine <jordan@strofina.com>
2017-02-07 17:34:44 -0800

    Add ferry_rider class

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -74,3 +74,40 @@ trix.speak(3)
 trix.roll_over
 trix.dog_years(50)
 trix.eat('kibble')
+
+
+# ferry rider class
+class Ferry_rider
+  def initialize
+    # this initialize method will print a statement to indicate this passenger is boarding the ferry
+    puts "I am boarding the ferry!"
+  end
+
+  def find_seat(introvert_level,commuter_boat_flag)
+    # this method will inspect the introvert level of this passenger (10 is very introverted, 1 is very extroverted)
+    # and the commuter boat flag (set to true if this is a commuter boat which is normally crowded), and it will 
+    # choose the seat that this passenger should look for
+    if commuter_boat_flag
+      return 'take first available seat'
+    else
+      case introvert_level
+        when 1..4
+          return 'sit in galley with talkers'
+        when 5..8
+          return 'sit in back of boat'
+        else
+          return 'go upstairs to quiet room'
+      end
+    end
+  end
+
+end
+
+jordan = Ferry_rider.new
+puts jordan.find_seat(5,false)
+mary = Ferry_rider.new
+puts mary.find_seat(8,true)
+tom = Ferry_rider.new
+puts tom.find_seat(9,false)
+simon = Ferry_rider.new
+puts simon.find_seat(1,false)
