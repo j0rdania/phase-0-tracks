01ffae9b263a52ba3b9e1d2f8c754d7035facc7b
Jordan Levine <jordan@strofina.com>
2017-02-07 18:45:28 -0800

    Add pretty print method

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -30,7 +30,27 @@
 # Method to print a list and make it look pretty
 # input: list name
 # steps:
-#     Print name of list
+
+def print_list(list_name)
 #     for each array element, print the item name and quantity
+      list_name.each { |list_item| puts "#{list_item[:item_name]}" + ':  ' + "#{list_item[:quantity]}"}
 #     Wish the shopper good luck.
+      puts "Happy Shopping!"
+end
 # output: array with all items on list
+
+jordan_list = [
+      { item_name: 'apples',
+        quantity: 3
+      },
+      { item_name: 'bananas',
+       quantity: 2
+      }
+    ]
+
+
+
+
+  puts jordan_list
+  puts "Our awesome method:"
+  print_list(jordan_list)
