c1955bb0f80587ce64125dede1f8fc1823da311e
Jordan Levine <jordan@strofina.com>
2017-02-07 16:54:13 -0800

    Add roll_over method

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -27,7 +27,30 @@ class Puppy
     toy
   end
 
+  # speak method - takes an integer and woofs that many times
+  # add carriage return to last woof
+  def speak(num_barks)
+    bark_count = 0
+    while bark_count < (num_barks - 1)
+      print "Woof! "
+      bark_count += 1
+    end
+    # one last woof with carriage return, as long as there was at least one woof
+    if num_barks > 0
+      puts "Woof!"
+    end
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end 
+
+
 end
 
 trix = Puppy.new
 trix.fetch('biscuit')
+trix.speak(0)
+trix.speak(1)
+trix.speak(3)
+trix.roll_over
