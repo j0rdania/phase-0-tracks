bae814fd8a40191991753a8b50a6e0253fb32c79
Jordan Levine <jordan@strofina.com>
2017-02-07 16:30:37 -0800

    Define puppy class - characteristics and behaviors

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+# Puppy class description
+
+# Characteristics:
+#   goofy_level: 9
+#   main_color: black
+#   name: Buddy
+#   has_owner: true
+#   pure_bred: false
+#   likes_kibble: true
+#   tricks_mastery_level:
+#       sit: 3
+#       down: 2
+#       stay: 1
+
+# Behaviors:
+#   eat
+#   bark
+#   sleep
+#   whine              
