7ab586b2189f2b330cacb10c44571bf1eb6cda79
Jordan Levine <jordan@strofina.com>
2017-02-09 18:25:00 -0800

    Add attributes and create several instances of Santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -4,8 +4,12 @@
 class Santa
 
   # define initialize method
-  def initialize
+  def initialize(gender,ethnicity)
     puts "Initializing Santa instance"
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
   # define speak method
   def speak
@@ -18,8 +22,21 @@ class Santa
   end
 end
 
-bob_santa = Santa.new()
-bob_santa.speak
-bob_santa.eat_milk_and_cookies('lemon jumble')
+# comment out this driver code because arguments have been added to initialize method
+# bob_santa = Santa.new
+# bob_santa.speak
+# bob_santa.eat_milk_and_cookies('lemon jumble')
 
 
+# some diverse initializations of Santa instances
+# declare empty array of santas
+santas = []
+# add first Santa
+santas.push(Santa.new('male','white'))
+# add 10 more Santas
+sample_ethnicities = ['Korean','Japanese','Indian','Unicorn']
+for santa_limit in 1..10
+  # pick random # between 0 and 3 - use that to define ethnicity
+  # use all male Santas - some things never change
+  santas.push(Santa.new('male',sample_ethnicities[rand(0..3)]))
+end
