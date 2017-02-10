5cca0d28e73a23c8ac8be9bdc3e8b1dbbb6c836d
Jordan Levine <jordan@strofina.com>
2017-02-09 19:39:52 -0800

    Add getter and setter methods and test

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -20,6 +20,50 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
   end
+
+  # method to print reindeer ranking
+  def print_reindeer_ranking
+    puts @reindeer_ranking
+  end
+  
+  # getter method to access gender from outside class
+  def gender
+    @gender
+  end
+
+  # three attribute changing methods:
+  # method to add one year to age
+  def celebrate_birthday
+    @age += 1
+  end
+
+  # method to move given reindeer to end of ranking
+  def get_mad_at(reindeer_name)
+    # find index where this reindeer is located and remove it
+    @reindeer_ranking.delete(reindeer_name)
+    # add it to the end of the array
+    @reindeer_ranking.push(reindeer_name)
+  end
+
+  # method to allow @gender to be reassigned from outside the class
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  # getter method to allow age to be accessed from outside the class
+  def age
+    @age
+  end
+  
+  # getter method to allow ethnicity to be accessed from outside the class
+  def ethnicity
+    @ethnicity
+  end
+  
+   # getter method to access gender from outside class
+  def gender
+    @gender
+  end
 end
 
 # comment out this driver code because arguments have been added to initialize method
@@ -40,3 +84,20 @@ for santa_limit in 1..10
   # use all male Santas - some things never change
   santas.push(Santa.new('male',sample_ethnicities[rand(0..3)]))
 end
+
+# driver code to test
+puts "1st Santa's age before birthday: " + santas[0].age.to_s
+santas[0].celebrate_birthday
+puts "1st Santa's age after birthday: " + santas[0].age.to_s
+puts "Reindeer ranking before Donner got caught: "
+santas[0].print_reindeer_ranking 
+santas[0].get_mad_at('Donner')
+puts "Reindeer ranking after Donner got caught: "
+santas[0].print_reindeer_ranking 
+puts "1st Santa's gender before change: #{santas[0].gender}"
+santas[0].gender = 'female'
+puts "1st Santa's gender after change: #{santas[0].gender}"
+puts "1st Santa's ethnicity: #{santas[0].ethnicity}"
+
+
+
