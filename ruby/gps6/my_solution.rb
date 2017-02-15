95317e5366fcd0a7e0966553c7e475aa9dcd9b97
Jordan Levine <jordan@strofina.com>
2017-02-15 15:53:23 -0800

    Refactor speed_of_spread

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -27,6 +27,8 @@ class VirusPredictor
 
   def predicted_deaths
     # predicted deaths is solely based on population density
+    # consider using case statement here - play with the math; look at structure - repeated .floor
+    # and assigning # of deaths
     if @population_density >= 200
       number_of_deaths = (@population * 0.4).floor
     elsif @population_density >= 150
@@ -46,18 +48,19 @@ class VirusPredictor
   def speed_of_spread    #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
+    # use case statement here
+    case @population_density
+      when 0..49
+        speed = 2.5
+      when 50..99
+        speed = 2
+      when 100..149
+        speed = 1.5
+      when 150..199
+        speed = 1
+      else
+        speed = 0.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
