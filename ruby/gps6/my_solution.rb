ad0dcd8fdad2acef6d9ac0fecbaf7ec1eefd1c09
Jordan Levine <jordan@strofina.com>
2017-02-15 16:34:41 -0800

    Add review and reflect and add method descriptions

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,7 +1,7 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge with Ransom Byers (John Gill guide).
+# We spent 2.5 hours on this challenge.
 
 # EXPLANATION OF require_relative
 # require_relative allows the code in this file to see the data/code that is in the referenced file.
@@ -11,12 +11,15 @@ require_relative 'state_data'
 
 class VirusPredictor
 
+  # accept state, population density, and population values
+  # and set instance variables - this method called upon .new
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  # call predicted_deaths and speed_of_spread methods
   def virus_effects
     predicted_deaths
     speed_of_spread
@@ -24,6 +27,7 @@ class VirusPredictor
 
   private
 
+  # calculate predicted deaths based on pop. density and print out results for this state
   def predicted_deaths
     # predicted deaths is solely based on population density
     case @population_density
@@ -45,6 +49,7 @@ class VirusPredictor
 
   end
 
+ # calculate speed of spread for this state based on pop. density
   def speed_of_spread    #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -86,3 +91,29 @@ end
 
 # alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 # alaska.virus_effects
+
+
+# REVIEW AND REFLECT
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+#   a. the one with the => (the outside hash) uses a string as the key; 
+#   the one with the : (the inside hash) uses a symbol as a key.
+#   b. therefore, the access the outside hash, we use a string (i.e. STATE_DATA['Alabama']); to access the 
+#   inside hash, we use a symbol (i.e. STATE_DATA['Alabama'][:population_density])
+# What does require_relative do? How is it different from require? 
+#   require_relative allows the code in the given section to reference code that is located in another file; 
+#   the "relative" part tells the code to start looking for the other code in the current directory,
+#   the directory that contains this code
+# What are some ways to iterate through a hash?
+#   a. you can use .each do |key, value|
+#   end, where the code to execute for each hash entry is between do and end
+#   b. you can use .each {|key,value| <code to execute here>} for shorter segments of code
+#   c. if you only need the keys, you can use .each_key instead of .each
+#   d. if you only need the values, you can use .each_value instead of .each
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+# The variables were instance variables, so there was no need to pass them into the method.
+# What concept did you most solidify in this challenge?
+# Having a hash within a hash was a bit tricky for me to wrap my brain around. My guide and my pair were both very helpful 
+# explaining the structure and various ways to access the hash elements.
+
+
+
