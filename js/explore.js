6b1af941d519f60176ba37a1bbea9e53b0bdc466
Jordan Levine <jordan@strofina.com>
2017-02-15 23:09:51 -0800

    Add pseudocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,12 @@
+//reverse function
+// this function will take a string as input and return that string in reverse order
+// for example, "hello" would be returned as "olleh"
+
+/* figure out length of string passed in
+   create an array of characters that holds the characters in the string passed in, in their original order
+   create a result string variable that starts as an empty string
+   for each element in the array, starting at the end of the array, add the letter in the array to our result string
+   continue until the 1st element of the array has been added to our result string
+   return the result string */
+
+   
