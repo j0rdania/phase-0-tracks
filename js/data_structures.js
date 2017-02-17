bde5082fd89afb6550c6e5cd8643cabe11fdfc84
Jordan Levine <jordan@strofina.com>
2017-02-17 00:05:43 -0800

    Create Car constructor function and build many cars on assembly line

--- js/data_structures.js
+++ js/data_structures.js
@@ -32,3 +32,27 @@ if (horse_names_length === horse_colors_length) {
 // test
 console.log('Bozo color in horses object is: '+ horses['Bozo']);
 
+// constructor function for car
+function Car(make,model,year,color) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.go = function() {console.log('The ' + this.year + this.make + this.model + ' is on its way!');};
+}
+// declare array of makes
+var makes = ['Acura','Toyota','GM'];
+// declare array of models
+var models = ['Torpedo','Slowpoke','Muffler Madness'];
+// declare array of years
+var years = [1994,2012,2017];
+// declare array of colors
+var colors = ['blue','black','red']
+
+// crank out some cars to sell; put them in an array of cars
+// declare empty array of cars for sale
+var cars_for_sale = [];
+for (var i = 0; i < makes.length; i++) {
+  cars_for_sale.push(new Car(makes[i],models[i],years[i],colors[i]));
+  console.log(cars_for_sale[i]);
+}
