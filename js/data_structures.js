a451c099ea2716b471373aec931b17e3c4696769
Jordan Levine <jordan@strofina.com>
2017-02-16 22:45:35 -0800

    Create horse color and name arrays and write code to add one element to each

---
+++ js/data_structures.js
@@ -0,0 +1,13 @@
+// declre array of horse colors with four values
+var horse_colors = ['tan','brown','black','white'];
+// declare array of horse names with four values
+var horse_names = ['Ed','Mary','Joe','Pickles'];
+
+// add another color to the horse color array
+horse_colors.push('purple');
+
+// add another name to the horse name array
+horse_names.push('Bozo');
+
+// test
+console.log('length of color array is ' + horse_colors.length);
