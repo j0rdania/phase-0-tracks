e6539f2ea24409be0aa4a383b4bdb571171e485b
Jordan Levine <jordan@strofina.com>
2017-03-05 15:45:17 -0800

    Add route to display student information for students with given first name

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -67,6 +67,33 @@ get '/add_two_numbers/:number1/:number2' do
   ((params[:number1].to_i) + (params[:number2].to_i)).to_s
 end
 
+# write a GET route to display full name, id, age, and campus for a given first name
+# first name is defined as all characters before the first space in the name passed in
+# this GET route will find all students whose name starts with the passed in first name
+# (so if Mil Smith is passed in, Mil would be the first name, and entries such as
+# Mil Jones, Mil Green, and Mildred Train would all be included in the result)
+# more than one row many be returned by this GET route
+# one param: name
+# GET /
+get '/students/with/first/name' do
+  first_name = params[:name].split(' ')[0]
+  first_name_wildcard= first_name + "%"
+  students_with_this_first_name = db.execute("select name, id, age, campus from students where name like '#{first_name_wildcard}';")
+  if students_with_this_first_name.length == 0
+    response = "Sorry, Charlie. There are no students with the first name #{first_name}."
+  else
+    response = "Students with a first name of #{first_name}:<br><br>"
+    students_with_this_first_name.each do |student|
+      response += "  Full Name: #{student['name'].to_s}<br>"
+      response += "  ID: #{student['id'].to_s}<br>"
+      response += "  Age: #{student['age'].to_s}<br>"   
+      response += "  Campus: #{student['campus']}<br><br>"
+    end
+    response += "<br>"
+  end
+  response
+end 
+
 
 
 
